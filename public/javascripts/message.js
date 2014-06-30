62ec94c58e85ca3966cc6194bd8ea330f8a9bad4
Romain Kurtz <kurtz.romain@gmail.com>
2014-06-30 12:30:22 +0200

    CSS for message

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -8,6 +8,7 @@ function SendMessage(){
 
 function popUpMessage(messageContent, mesh){
 	var element;
+	var elem;
 	var messagePosition;
 
 
@@ -30,16 +31,40 @@ function popUpMessage(messageContent, mesh){
 
 
 				element	= document.createElement('div')
-				document.body.appendChild(element)
-				element.innerHTML = "<center>"+messageContent+"</center>";
-				element.style.border	= '0'
-				element.style.height	= '142px';
+				document.body.appendChild(element);
+				element.innerHTML = messageContent;
+				element.style.border	= '0';
+				element.style.height	= '50px';
 				element.style.color = '#E9E9E9'
-				element.style.width	= '200px';
+				element.style.width	= '250px';
 				element.style.overflow	= 'hidden'
 				element.style.position	= 'absolute'
-				element.frameBorder	= '1'
+				element.style.color ="#09C"
+				element.className = 'toto';
+				element.style.opacity = "0";
+
+				//element.frameBorder	= '1'
+				//element.className ="message"
 				//console.log(messageContent);
+				//var elem = document.getElementsByClassName ("message")[0];
+				//console.log(document.getElementsByClassName ("message"));
+				//elem.style.webkitTransform = "scale(1) rotate(-10deg)";
+				//elem.style.transition = "all (.25s)";
+				//elem.style.opacity = "1";
+				//elem.style.webkitTransform = "scale(2) rotate(0deg)";
+
+				//console.log(element);
+				//element = document.getElementsByClassName ("toto")[0];
+				//console.log(element);
+				setTimeout(function () { // Need this for animation CSS
+					element.style.opacity = "1";
+					element.style.background = "rgba(170, 170, 170, 0.168627)";
+					element.style.webkitTransform = "scale(1) rotate(0)";
+  				 }, 1);
+
+				
+
+
 
 
 
@@ -47,13 +72,24 @@ function popUpMessage(messageContent, mesh){
             .onUpdate( function () {
 
             	messagePosition	= THREEx.ObjCoord.cssPosition(mesh, camera,renderer)
-				element.style.left	= (messagePosition.x-element.offsetWidth /2)+'px';
-				element.style.top	= (messagePosition.y-0-element.offsetHeight/2)+'px';
+				element.style.left	= (messagePosition.x-0-element.offsetWidth /2)+'px';
+				element.style.top	= (messagePosition.y+20-element.offsetHeight/2)+'px';
 
 
             })
             .onComplete( function () {
-            	element.parentNode.removeChild(element);
+            	element.style.opacity = "0";
+				element.style.webkitTransform = "scale(0) rotate(-12deg)";
+            	var nb_event = 0;
+            	element.addEventListener( 'webkitTransitionEnd', function( event ) { 
+            		nb_event++;
+            		if (nb_event==2)    // Number of CSS Transition Event for delete DIV
+            		{
+            			var aux = element.parentNode;
+						aux.removeChild(element);
+					}
+				}, false );
+            	
             })
             .start();
 
@@ -67,7 +103,7 @@ function createTextAreaMessage(mesh){
 
 				textArea	= document.createElement('div')
 				document.body.appendChild(textArea)
-				textArea.innerHTML = "<input type=\"text\" class=\"form-control\" id= \"textAreaMessage\" placeholder=\"Type your message\">";
+				textArea.innerHTML = "<input type=\"text\" maxlength=\"30\" class=\"form-control\" id= \"textAreaMessage\" placeholder=\"Type your message\">";
 				//textArea.innerHTML = "<center>COUOUC !!!</center>";
 				textArea.style.border	= '0'
 				textArea.style.height	= '142px';
